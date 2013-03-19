2cd4559280d304a9075bb705f1abe5c1b3c9cd52
jonyoder <jonyoder@gmail.com>
2013-03-19 12:19:19 -0500

    Update Client Interceptor to Optionally Clear Form Parameters (for GWT apps).

--- cashmac-client/src/main/java/edu/swmed/qbrc/auth/cashmac/client/ClientAuthInterceptor.java
+++ cashmac-client/src/main/java/edu/swmed/qbrc/auth/cashmac/client/ClientAuthInterceptor.java
@@ -15,12 +15,26 @@ public class ClientAuthInterceptor implements ClientExecutionInterceptor {
 	String hostName = null;
 	String clientId = null;
 	String secret = null;
+	Boolean needsFormParametersCleared = false;
 	
 	@SuppressWarnings("rawtypes")
 	public ClientResponse execute(ClientExecutionContext context) throws Exception {
 		
+		// Hack for removing form parameters for GWT applications.  GWT apparently adds form params to requests,
+		// and if RESTEasy sees any form parameters, it will assume that we need an HttpPost at some point, which
+		// will result in an error like:
+		//   org.apache.http.client.methods.HttpGet cannot be cast to org.apache.http.client.methods.HttpPost
+		if (needsFormParametersCleared) {
+			context.getRequest().getFormParameters().clear();
+		}
+
 		HMACUtils.createSignatureAndSignRequest(context.getRequest(), hostName, clientId, secret);
+		
 		return context.proceed();
+	}
+	
+	public void setNeedsFormParametersCleared(Boolean needsFormParametersCleared) {
+		this.needsFormParametersCleared = needsFormParametersCleared;
 	}
 	
 	public void setHostName(String hostName) {
