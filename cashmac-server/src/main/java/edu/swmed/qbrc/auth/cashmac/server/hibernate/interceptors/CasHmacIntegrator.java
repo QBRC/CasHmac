cf4d2ec33e301d557e210e29fec1e64f7e5fa6ed
jonyoder <jonyoder@gmail.com>
2013-05-14 12:16:56 -0500

    Update comments for CasHmac library.

--- cashmac-server/src/main/java/edu/swmed/qbrc/auth/cashmac/server/hibernate/interceptors/CasHmacIntegrator.java
+++ cashmac-server/src/main/java/edu/swmed/qbrc/auth/cashmac/server/hibernate/interceptors/CasHmacIntegrator.java
@@ -8,6 +8,27 @@ import org.hibernate.integrator.spi.Integrator;
 import org.hibernate.metamodel.source.MetadataImplementor;
 import org.hibernate.service.spi.SessionFactoryServiceRegistry;
 
+/**
+ * Here, we define some event handlers for Hibernate.  Please see also
+ * the CasHmacInterceptor class, where we configure "pre" intereceptors
+ * for checking ACLs.  The intereceptors defined here are for "post" 
+ * events, like inserting ACLs for new objects and deleting old ACLs
+ * for deleted objects.
+ * 
+ * Please note that to enable this custom Integrator, you need to create 
+ * a file (on your server) at "META-INF/services" named
+ * "org.hibernate.integrator.spi.Integrator" and enter the following
+ * single line in the file:
+ * 
+ *     edu.swmed.qbrc.auth.cashmac.server.hibernate.interceptors.CasHmacIntegrator
+ * 
+ * Be sure that the file referenced above is properly deployed to your
+ * server (packaged in the .WAR file), or these event handlers won't be
+ * used.
+ * 
+ * @author JYODE1
+ *
+ */
 public class CasHmacIntegrator implements Integrator {
 
 	@Override
@@ -17,7 +38,9 @@ public class CasHmacIntegrator implements Integrator {
 	@Override
 	public void integrate(Configuration config, SessionFactoryImplementor sessionFactoryImplementor, SessionFactoryServiceRegistry registry) {
 		final EventListenerRegistry eventRegistry = registry.getService(EventListenerRegistry.class);
+		// Runs after a new entity is INSERTED.
 		eventRegistry.prependListeners(EventType.POST_INSERT, new CasHmacPostInsertListener());
+		// Runs after an entity is DELETED.
 		eventRegistry.prependListeners(EventType.POST_DELETE, new CasHmacPostDeleteListener());
 	}
 
--- cashmac-server/src/main/java/edu/swmed/qbrc/auth/cashmac/server/hibernate/interceptors/CasHmacInterceptor.java
+++ cashmac-server/src/main/java/edu/swmed/qbrc/auth/cashmac/server/hibernate/interceptors/CasHmacInterceptor.java
@@ -16,6 +16,18 @@ import edu.swmed.qbrc.auth.cashmac.server.guice.MainGuiceModule;
 import edu.swmed.qbrc.auth.cashmac.server.hibernate.exceptions.NoAclException;
 import edu.swmed.qbrc.auth.cashmac.shared.constants.CasHmacAccessLevels;
 
+/**
+ * Here we intercept Hibernate events. These are all "pre" interceptors, meaning that
+ * they run before the pending action takes place.  Hence, we use these interceptors
+ * to check for the necessary ACLs before performing the action.
+ * 
+ * For actions that require "post" interceptors, please see the CasHmacIntegrator class,
+ * which sets up post delete and post insert interceptors (to delete ACLs and insert new
+ * ACLs, respectively).
+ * 
+ * @author JYODE1
+ *
+ */
 public class CasHmacInterceptor extends EmptyInterceptor {
 
 	@Inject CrudAclSearchFactory crudAclSearchFactory;
@@ -30,6 +42,9 @@ public class CasHmacInterceptor extends EmptyInterceptor {
 		Guice.createInjector(guiceModule).injectMembers(this);
 	}
 	
+	/**
+	 * Before an entity is INSERTED.
+	 */
 	@Override
 	public boolean onSave(Object entity, Serializable id, Object[] state, String[] propertyNames, Type[] types) {
 		log.trace("Created: " + entity.getClass().getName() + "\nFrom Session: " + CasHmacRequestFilter.getSession().getId());
@@ -40,7 +55,10 @@ public class CasHmacInterceptor extends EmptyInterceptor {
 			throw new NoAclException("No ACL to insert entity of type " + entity.getClass().getSimpleName());
 		}
 	}
-	
+
+	/**
+	 * Before an entity is LOADED.
+	 */
 	@Override
 	public boolean onLoad(Object entity, Serializable id, Object[] state, String[] propertyNames, Type[] types) {
 		log.trace("Read: " + entity.getClass().getName() + "\nFrom Session: " + CasHmacRequestFilter.getSession().getId() + "\nNumber of Properties: " + propertyNames.length);
@@ -52,6 +70,9 @@ public class CasHmacInterceptor extends EmptyInterceptor {
 		}
 	}
 
+	/**
+	 * Before an existing entity is UPDATED.
+	 */
 	@Override
 	public boolean onFlushDirty(Object entity, Serializable id, Object[] currentState, Object[] previousState, String[] propertyNames, Type[] types) {
 		log.trace("Updated: " + entity.getClass().getName() + "\nFrom Session: " + CasHmacRequestFilter.getSession().getId());
@@ -63,6 +84,9 @@ public class CasHmacInterceptor extends EmptyInterceptor {
 		}
 	}
 
+	/**
+	 * Before an entity is DELETED.
+	 */
 	@Override
 	public void onDelete(Object entity, Serializable id, Object[] state, String[] propertyNames, Type[] types) {
 		log.trace("Deleted: " + entity.getClass().getName() + "\nFrom Session: " + CasHmacRequestFilter.getSession().getId());
